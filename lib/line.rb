69ee937ce834ca32d4ef677cb9f30d335f5b3420
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2015-06-22 20:09:39 +0530

    Some changes with specs

--- lib/line.rb
+++ lib/line.rb
@@ -1,13 +1,17 @@
 class Line
-  def initialize(initial_x_coordinate,initial_y_coordinate,final_x_coordinate,final_y_coordinate)
-   @initial_x_coordinate=initial_x_coordinate
-   @initial_y_coordinate=initial_y_coordinate
-   @final_x_coordinate=final_x_coordinate
-   @final_y_coordinate=final_y_coordinate
+  attr_accessor :initial_x_coordinate, :initial_y_coordinate, :final_x_coordinate, :final_y_coordinate
+  
+  def initialize(initial_x_coordinate, initial_y_coordinate, final_x_coordinate, final_y_coordinate)
+    @initial_x_coordinate = initial_x_coordinate
+    @initial_y_coordinate = initial_y_coordinate
+    @final_x_coordinate = final_x_coordinate
+    @final_y_coordinate = final_y_coordinate
   end
-  def distance
-    distance = Math.sqrt((@initial_x_coordinate-@final_x_coordinate) ** 2 + (@initial_y_coordinate-@final_y_coordinate) ** 2 )
-    puts "distance: #{distance}"
-    return distance
+
+ def distance
+   distance = Math.sqrt((self.initial_x_coordinate - self.final_x_coordinate) ** 2 + (self.initial_y_coordinate - self.final_y_coordinate) ** 2 )
+   puts "distance: #{distance}"
+   return distance
   end
 end
+
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-
 describe "distance" do
   it "takes co-ordinates and calculates the distance" do
-    expect(Line.new(3,2,9,7).distance).to eq Math.sqrt((3-9) ** 2 + (2-7) ** 2 )
+    line  = Line.new(2, 3, 7, 9)
+
+    expect(line.distance).to eq Math.sqrt((3-9) ** 2 + (2-7) ** 2 )
   end
 end
