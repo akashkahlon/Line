e7091e024578876c56c39796426d216080b5b641
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2015-06-23 17:39:57 +0530

    Slope and Orthogonality Function added

--- lib/line.rb
+++ lib/line.rb
@@ -32,4 +32,27 @@ class Line
       return false
     end
   end
+
+  def calculate_slope
+    if self.final_point.x_coordinate - self.initial_point.x_coordinate == 0
+      slope = "undefined"
+      return slope
+    else
+      slope = ( self.final_point.y_coordinate - self.initial_point.y_coordinate ).to_f / ( self.final_point.x_coordinate - self.initial_point.x_coordinate)
+      return slope
+    end
+  end
+
+  def self.orthogonality_check(line_one, line_two)
+    slope_of_line_one = line_one.calculate_slope
+    slope_of_line_two = line_two.calculate_slope
+    if slope_of_line_two == 0 && slope_of_line_one == 'undefined' || slope_of_line_one == 0 && slope_of_line_two == 'undefined'
+      return true
+    elsif line_one.calculate_slope * line_two.calculate_slope == -1
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -11,9 +11,9 @@ describe "distance" do
 end
 
 describe "line_equality" do
-    initial_point = Point.new(2,3)
-    final_point = Point.new(7,9)
-    third_point = Point.new(6,7)
+  initial_point = Point.new(2,3)
+  final_point = Point.new(7,9)
+  third_point = Point.new(6,7)
   it "should return true if two lines have sam starting and end points" do
     line_one = Line.new(initial_point, final_point)
     line_two = Line.new(initial_point, final_point)
@@ -34,9 +34,9 @@ describe "line_equality" do
 end
 
 describe "line_equality_without_self" do
-    initial_point = Point.new(2,3)
-    final_point = Point.new(7,9)
-    third_point = Point.new(6,7)
+  initial_point = Point.new(2,3)
+  final_point = Point.new(7,9)
+  third_point = Point.new(6,7)
   it "should return true if two lines have sam starting and end points" do
     line_one = Line.new(initial_point, final_point)
     line_two = Line.new(initial_point, final_point)
@@ -53,5 +53,37 @@ describe "line_equality_without_self" do
     line_one = Line.new(initial_point, final_point)
     line_two = Line.new(final_point, initial_point)
     expect(line_one.line_equality_without_self(line_two)).to eq true
+  end
+end
+
+describe "calculate_slope" do
+  it "calculates the slope of a line" do
+    initial_point = Point.new(2,3)
+    final_point = Point.new(7,9)
+    expect(Line.new(initial_point, final_point).calculate_slope).to eq (9-3).to_f / (7-2)
+  end
+
+  it "calculates the slope of a line for vertical line" do
+    initial_point = Point.new(2,3)
+    final_point = Point.new(2,9)
+    expect(Line.new(initial_point, final_point).calculate_slope).to eq 'undefined'
+  end
+end
+
+describe "orthogonality check" do
+  initial_point_1 = Point.new(0,-4)
+  final_point_1 = Point.new(-1,-7)
+  initial_point_2 = Point.new(3,0)
+  final_point_2 = Point.new(-3,2)
+
+  it "returns true if 2 lines are othogonal" do
+    line_one=Line.new(initial_point_1, final_point_1)
+    line_two=Line.new(initial_point_2, final_point_2)
+    expect(Line.orthogonality_check(line_one, line_two)).to eq true
+  end
+  it "returns false if 2 lines are not orthogonal" do
+    line_one=Line.new(initial_point_1, initial_point_2)
+    line_two=Line.new(initial_point_1, final_point_2)
+    expect(Line.orthogonality_check(line_one, line_two)).to eq false
   end
 end
