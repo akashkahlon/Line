d0d74bd46f8ed502340aa4190a347ae0d93c3875
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2015-06-24 13:23:40 +0530

    Rectangle class

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,17 +1,29 @@
 class Rectangle
-  #attr_accessor :point_one, :point_two, :point_three, :point:four, :length, :breadth
+  attr_accessor :point_one, :point_two, :point_three, :point_four
 
-  def intialize(point_one, point_two, point_three, point_four)
+  def initialize(point_one, point_two, point_three, point_four)
     @point_one = point_one
     @point_two = point_two
     @point_three = point_three
     @point_four = point_four
   end
 
+  def self.new_initialize(length, breadth)
+    point_one = Point.new(0, 0)
+    point_two = Point.new(0, length)
+    point_three = Point.new(breadth, length)
+    point_four = Point.new(breadth,0)
+    Rectangle.new(point_one, point_two, point_three, point_four)
+  end
+
   def calculate_area
-    area = ( self.length_line.distance * self.breadth_line.distance )
+    area = ( Line.new(@point_one, @point_two).distance * Line.new(@point_one, @point_four).distance )
     return area
   end
 
+  def calculate_perimeter
+    perimeter = 2 * (Line.new(@point_one, @point_two).distance +  Line.new(@point_one, @point_four).distance )
+    return perimeter
+  end
 end
 
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,1 +1,62 @@
+require 'spec_helper'
 
+describe "calculate_area" do
+  it "takes 4 points and outputs the area the area of the rectangle" do
+    point_one = Point.new(0,0)
+    point_two = Point.new(0,5)
+    point_three = Point.new(4,5)
+    point_four = Point.new(4,0)
+    expect(Rectangle.new(point_one, point_two, point_three, point_four).calculate_area).to eq 20
+  end
+  it "takes 2 inputs, length and breadth and calculates the output" do
+    length = 5
+    breadth = 4
+    expect(Rectangle.new_initialize(length, breadth).calculate_area).to eq 20
+  end
+end
+describe "initialize" do
+  it "takes 4 points as input" do
+    point_one=Point.new(0,0)
+    point_two=Point.new(0,5)
+    point_three=Point.new(4,5)
+    point_four=Point.new(4,0)
+    rectangle = Rectangle.new(point_one, point_two, point_three, point_four)
+    expect(rectangle.point_one.x_coordinate).to eq 0
+    expect(rectangle.point_one.y_coordinate).to eq 0
+    expect(rectangle.point_two.x_coordinate).to eq 0
+    expect(rectangle.point_two.y_coordinate).to eq 5
+    expect(rectangle.point_three.x_coordinate).to eq 4
+    expect(rectangle.point_three.y_coordinate).to eq 5
+    expect(rectangle.point_four.x_coordinate).to eq 4
+    expect(rectangle.point_four.y_coordinate).to eq 0
+  end
+end
+describe "new_initialize" do
+  it "takes length and breadth as input and gives 4 points" do
+    length = 5
+    breadth = 4
+    rectangle = Rectangle.new_initialize(length, breadth)
+    expect(rectangle.point_one.x_coordinate).to eq 0
+    expect(rectangle.point_one.y_coordinate).to eq 0
+    expect(rectangle.point_two.x_coordinate).to eq 0
+    expect(rectangle.point_two.y_coordinate).to eq 5
+    expect(rectangle.point_three.x_coordinate).to eq 4
+    expect(rectangle.point_three.y_coordinate).to eq 5
+    expect(rectangle.point_four.x_coordinate).to eq 4
+    expect(rectangle.point_four.y_coordinate).to eq 0
+  end
+end
+describe "perimeter"do
+  it "takes four points and calculates the perimeter of the rectangle " do
+    point_one = Point.new(0,0)
+    point_two = Point.new(0,5)
+    point_three = Point.new(4,5)
+    point_four = Point.new(4,0)
+    expect(Rectangle.new(point_one, point_two, point_three, point_four).calculate_perimeter).to eq 18
+  end
+  it "takes length and breadth and calculates perimeter of rectangle" do
+    length = 5
+    breadth = 4
+    expect(Rectangle.new_initialize(length, breadth).calculate_perimeter).to eq 18
+  end
+end
