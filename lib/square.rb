99d7065f029ca989b9f03c83b4ea83d3b3ba608f
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2015-06-24 15:26:40 +0530

    Square class with some changes

--- lib/square.rb
+++ lib/square.rb
@@ -1,10 +1,6 @@
 class Square < Rectangle
 
   def self.new_initialize(side)
-    point_one = Point.new(0, 0)
-    point_two = Point.new(0, side)
-    point_three = Point.new(side, side)
-    point_four = Point.new(side,0)
-    Rectangle.new(point_one, point_two, point_three, point_four)
+    super(side, side)
   end
 end
